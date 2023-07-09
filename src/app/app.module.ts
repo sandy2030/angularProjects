4c17d8a97b65a2991952741bf753fb1009027466
sandy2030 <sandeeppandey2024@gmail.com>
2023-07-10 01:02:35 +0530

    first angular commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeftSideComponent } from './left-side/left-side.component';
+import { RightSideComponent } from './right-side/right-side.component';
+import { HeaderComponent } from './header/header.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSideComponent,
+    RightSideComponent,
+    HeaderComponent,
+    MainBodyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/left-side/left-side.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LeftSideComponent } from './left-side.component';
+
+describe('LeftSideComponent', () => {
+  let component: LeftSideComponent;
+  let fixture: ComponentFixture<LeftSideComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LeftSideComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LeftSideComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
